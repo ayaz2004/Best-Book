842df825be91992c395a4930c16903599f2af74f
ayaz2004 <ayaznazir03@gmail.com>
2024-12-30 21:33:57 +0530

    Created user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    currentClass: {
+      type: String,
+      required: true,
+    },
+    targetExam: {
+      type: String,
+      required: true,
+    },
+    targetYear: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
