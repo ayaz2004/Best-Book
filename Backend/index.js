a97dba80ae164e256c5a646a0706e5f3ade4cd39
ayaz2004 <ayaznazir03@gmail.com>
2024-12-30 21:00:05 +0530

    First Commit

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!");
+});
