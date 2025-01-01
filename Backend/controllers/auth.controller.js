7d41dcc1cf6fbef77c15693670e9821cdec4dca8
ayaz2004 <ayaznazir03@gmail.com>
2025-01-02 00:03:00 +0530

    Completed singin API route

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const {
@@ -42,6 +43,35 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("Sign Up successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password || username === "" || password === "") {
+    next(errorhandler(400, "All fields are required."));
+  }
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) {
+      return next(errorhandler(404, "Wrong Credentials"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorhandler(400, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
