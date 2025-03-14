b577fd72643b1029137cb9ac7270bdf4760cd9ba
ayaz2004 <ayaznazir03@gmail.com>
2025-03-15 00:30:37 +0530

    Add top banners endpoint: implement GET route to retrieve top banners

--- Backend/routes/banners.route.js
+++ Backend/routes/banners.route.js
@@ -1,13 +1,24 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
-import { uploadBanner } from "../controllers/banner.controller.js";
+import {
+  getTopBanners,
+  uploadBanner,
+} from "../controllers/banner.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
 
-router.post("/uploadbanners",verifyToken,
-    upload.fields([{
-        name:"banners",
-        maxCount:1
-    }]), uploadBanner);
+router.post(
+  "/uploadbanners",
+  verifyToken,
+  upload.fields([
+    {
+      name: "banners",
+      maxCount: 1,
+    },
+  ]),
+  uploadBanner
+);
 
-export default router
+router.get("/topBanners", getTopBanners);
+
+export default router;
